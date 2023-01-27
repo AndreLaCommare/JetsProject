2c553592e36f650f6220766ee6ca29da8faf2dbc
andre_lacommare <alacommaresb@gmail.com>
2023-01-27 15:18:44 -0800

    Create Basic Structure

---
+++ src/com/skilldistillery/app/JetsApp.java
@@ -0,0 +1,18 @@
+package com.skilldistillery.app;
+
+import com.skilldistillery.entities.Airfield;
+
+public class JetsApp {
+	
+	private Airfield airF;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void run() {
+		
+	}
+
+}
---
+++ src/com/skilldistillery/entities/Airfield.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.entities;
+
+import java.util.List;
+
+public class Airfield {
+
+	private List<Jet> jets;
+	
+	public void showFastestJet() {
+		
+	}
+
+}
